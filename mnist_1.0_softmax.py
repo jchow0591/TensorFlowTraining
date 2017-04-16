2c57c895a631f005c6d3fd2ca5de15ecf39716cd
Jeffrey Chow <jeffreychow@Jeffreys-MBP.home>
2017-04-16 17:54:57 -0400

    Initial Commit

--- mnist_1.0_softmax.py
+++ mnist_1.0_softmax.py
@@ -47,6 +47,7 @@ W = tf.Variable(tf.zeros([784, 10]))
 # biases b[10]
 b = tf.Variable(tf.zeros([10]))
 
+
 # flatten the images into a single line of pixels
 # -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
 XX = tf.reshape(X, [-1, 784])
